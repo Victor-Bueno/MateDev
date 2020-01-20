c04dff29d70b68c546f9c06fd5d2e7fb9081a12a
Victor-Bueno <victor.bueno0720@gmail.com>
2020-01-20 20:21:05 -0300

    Adding real time features

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose'); // MongoDB (noSQL)
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://Victor:Vi14042010@cluster0-z8sgm.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +19,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
